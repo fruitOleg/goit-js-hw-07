5949a4d97899f26c25d8e10a41afd54311ca14c1
not oleg <104638867+fruitOleg@users.noreply.github.com>
2023-06-10 18:54:38 +0200

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const listGallery = document.querySelector(".gallery");
+
+const createCardPicture = galleryItems.map(
+  ({ preview, original, description }) => {
+    return ` 
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  }
+);
+
+listGallery.insertAdjacentHTML("beforeend", createCardPicture.join(""));
